be93d778cbb0b8a22e09ae755a6b180d32b254e4
himanshu-antal <himanshuantal26@gmail.com>
2026-02-27 16:58:35 +0530

    mostly complete

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,18 +1,40 @@
+// const jwt = require("jsonwebtoken");
+
+// exports.protect = (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization?.split(" ")[1];
+
+//     if (!token)
+//       return res.status(401).json({ message: "Unauthorized" });
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ message: "Invalid or Expired Token" });
+//   }
+// };
+
 const jwt = require("jsonwebtoken");
 
 exports.protect = (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(" ")[1];
-
-    if (!token)
-      return res.status(401).json({ message: "Unauthorized" });
-
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+    
+    const token = authHeader.split(" ")[1];
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-
+    
+    req.user = decoded.id;
+    
     next();
-  } catch (err) {
-    res.status(401).json({ message: "Invalid or Expired Token" });
+  } catch (error) {
+    return res.status(401).json({ message: "Token invalid" });
   }
 };
 
